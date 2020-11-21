38eda4dd4374adb07b07310a8958e6818a000a91
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2020-11-21 17:04:44 +0530

    abserver tweaks.

--- tests/abserver.tcl
+++ tests/abserver.tcl
@@ -1,6 +1,3 @@
-package require iocp_inet
-package require Iocpsock
-
 proc readrequest {chan} {
     read $chan
     puts -nonewline $chan "HTTP/1.1 200 OK\r\n\r\n<!doctype html><html></html>"
@@ -13,8 +10,29 @@ proc accept {chan addr port} {
 }
 
 fconfigure [socket -server accept 8080] {*}$argv
-fconfigure [iocp::inet::socket -server accept 8081] {*}$argv -maxpendingaccepts 5
-fconfigure [socket2 -server accept 8082] {*}$argv
+puts stderr "Tcl socket listening on 8082"
+
+if {[catch {
+    package require iocp_inet
+    fconfigure [iocp::inet::socket -server accept 8081] {*}$argv
+} result]} {
+    puts stderr "iocp::inet disabled: $result"
+} else {
+    puts stderr "iocp::inet listening on 8081"
+}
+
+if {[catch {
+    package require Iocpsock
+    fconfigure [socket2 -server accept 8082] {*}$argv
+} result]} {
+    puts stderr "Iocpsock disabled: $result"
+} else {
+    puts stderr "Iocpsock listening on 8082"
+}
+
+puts stderr "Client command examples:"
+puts stderr "  ab -n COUNT -c CONCURRENCY http://localhost:8081/"
+puts stderr "  ab -t SECS -c CONCURRENCY http://localhost:8081/"
 
 if {[file normalize [info script]/...] eq [file normalize $argv0/...]} {
     vwait forever    
